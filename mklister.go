9a506ef695b50070604a48def0f04dd81ea50b10
Max Kohl <maxkohl810@gmail.com>
2016-07-19 13:50:36 -0400

    Recursively print directory contents if recursive flag is passed in

--- mklister.go
+++ mklister.go
@@ -9,8 +9,32 @@ import (
 	"gopkg.in/urfave/cli.v1"
 )
 
+func PrintContents(path string) error {
+	files, err := ioutil.ReadDir(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if file.IsDir() {
+			fmt.Println(file.Name())
+			PrintContents(path + "/" + file.Name())
+		} else {
+			fmt.Println(file.Name())
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	var directory string
+	var recursive bool
 
 	app := cli.NewApp()
 	app.Name = "mklister"
@@ -25,6 +49,7 @@ func main() {
 		cli.BoolFlag{
 			Name: "recursive, r",
 			Usage: "when set, list files recursively. default is off",
+			Destination: &recursive,
 		},
 		cli.StringFlag{
 			Name: "output, o",
@@ -33,14 +58,18 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
-		files, err := ioutil.ReadDir(directory)
+		if recursive {
+			PrintContents(directory)
+		} else {
+			files, err := ioutil.ReadDir(directory)
 
-		if err != nil {
-			log.Fatal(err)
-		}
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		for _, file := range files {
-			fmt.Println(file.Name())
+			for _, file := range files {
+				fmt.Println(file.Name())
+			}
 		}
 
 		return nil
