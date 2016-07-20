60cc39d5538d304bb76c9fe590889f55853aa1a6
Max Kohl <maxkohl810@gmail.com>
2016-07-19 20:35:38 -0400

    Fix indentation issues

--- mklister.go
+++ mklister.go
@@ -1,205 +1,205 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"encoding/json"
-	"time"
+  "os"
+  "fmt"
+  "io/ioutil"
+  "log"
+  "encoding/json"
+  "time"
 
-	"gopkg.in/urfave/cli.v1"
+  "gopkg.in/urfave/cli.v1"
 )
 
 type AsJSON struct {
-	Name string
-	Size int64
-	ModifiedTime time.Time
-	IsLink bool
-	IsDir bool
-	LinksTo string
-	Children []AsJSON
+  Name string
+  Size int64
+  ModifiedTime time.Time
+  IsLink bool
+  IsDir bool
+  LinksTo string
+  Children []AsJSON
 }
 
 func PrintContents(path string, level int) error {
-	files, err := ioutil.ReadDir(path)
+  files, err := ioutil.ReadDir(path)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	for _, file := range files {
+  for _, file := range files {
 
-		if file.IsDir() {
-			indentation := ""
+    if file.IsDir() {
+      indentation := ""
 
-			for i := 0; i < level; i++ {
-				indentation += " "
-			}
+      for i := 0; i < level; i++ {
+        indentation += " "
+      }
 
-			fmt.Println(indentation + file.Name() + "/")
+      fmt.Println(indentation + file.Name() + "/")
 
-			PrintContents((path + "/" + file.Name()), (level + 1))
+      PrintContents((path + "/" + file.Name()), (level + 1))
 
-		} else {
-			indentation := " "
+    } else {
+      indentation := " "
 
-			for i := 0; i < level; i++ {
-				indentation += " "
-			}
+      for i := 0; i < level; i++ {
+        indentation += " "
+      }
 
-			symPath := SymLink(file, path)
+      symPath := SymLink(file, path)
 
-			if symPath != "" {
-				fmt.Println(indentation + file.Name() + "*" + " (" + symPath + ")")
-			} else {
-				fmt.Println(indentation + file.Name())
-			}
-		}
-	}
+      if symPath != "" {
+        fmt.Println(indentation + file.Name() + "*" + " (" + symPath + ")")
+      } else {
+        fmt.Println(indentation + file.Name())
+      }
+    }
+  }
 
-	return nil
+  return nil
 }
 
 func PrepareJSONNonRecursive(path string) []AsJSON {
-	files, err := ioutil.ReadDir(path)
+  files, err := ioutil.ReadDir(path)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	list := []AsJSON{}
+  list := []AsJSON{}
 
-	for _, file := range files {
-		formatted := AsJSON{
-			Name: file.Name(),
-			Size: file.Size(),
-			ModifiedTime: file.ModTime(),
-			IsLink: (file.Mode() & os.ModeSymlink == os.ModeSymlink),
-			IsDir: file.IsDir(),
-			LinksTo: SymLink(file, path),
-		}
-		list = append(list, formatted)
-	}
+  for _, file := range files {
+    formatted := AsJSON{
+      Name: file.Name(),
+      Size: file.Size(),
+      ModifiedTime: file.ModTime(),
+      IsLink: (file.Mode() & os.ModeSymlink == os.ModeSymlink),
+      IsDir: file.IsDir(),
+      LinksTo: SymLink(file, path),
+    }
+    list = append(list, formatted)
+  }
 
-	return list
+  return list
 }
 
 func PrepareJSON(path string) []AsJSON {
-	files, err := ioutil.ReadDir(path)
+  files, err := ioutil.ReadDir(path)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	list := []AsJSON{}
+  list := []AsJSON{}
 
-	for _, file := range files {
-		formatted := AsJSON{}
+  for _, file := range files {
+    formatted := AsJSON{}
 
-		if file.IsDir() {
-			formatted = AsJSON{
-					Name: file.Name(),
-					Size: file.Size(),
-					ModifiedTime: file.ModTime(),
-					IsLink: (file.Mode() & os.ModeSymlink == os.ModeSymlink),
-					IsDir: file.IsDir(),
-					LinksTo: SymLink(file, path),
-					Children: PrepareJSON(path + "/" + file.Name()),
-			}
-		} else {
-			formatted = AsJSON{
-					Name: file.Name(),
-					Size: file.Size(),
-					ModifiedTime: file.ModTime(),
-					IsLink: (file.Mode() & os.ModeSymlink == os.ModeSymlink),
-					IsDir: file.IsDir(),
-					LinksTo: SymLink(file, path),
-				}
-		}
-		list = append(list, formatted)
-	}
+    if file.IsDir() {
+      formatted = AsJSON{
+          Name: file.Name(),
+          Size: file.Size(),
+          ModifiedTime: file.ModTime(),
+          IsLink: (file.Mode() & os.ModeSymlink == os.ModeSymlink),
+          IsDir: file.IsDir(),
+          LinksTo: SymLink(file, path),
+          Children: PrepareJSON(path + "/" + file.Name()),
+      }
+    } else {
+      formatted = AsJSON{
+          Name: file.Name(),
+          Size: file.Size(),
+          ModifiedTime: file.ModTime(),
+          IsLink: (file.Mode() & os.ModeSymlink == os.ModeSymlink),
+          IsDir: file.IsDir(),
+          LinksTo: SymLink(file, path),
+        }
+    }
+    list = append(list, formatted)
+  }
 
-	return list
+  return list
 }
 
 func SymLink (file os.FileInfo, path string) string {
-	if file.Mode() & os.ModeSymlink == os.ModeSymlink {
-		symPath, err := os.Readlink(path + "/" + file.Name())
+  if file.Mode() & os.ModeSymlink == os.ModeSymlink {
+    symPath, err := os.Readlink(path + "/" + file.Name())
 
-		if err != nil {
-			log.Fatal(err)
-		}
+    if err != nil {
+      log.Fatal(err)
+    }
 
-		return symPath
-	}
+    return symPath
+  }
 
-	return ""
+  return ""
 }
 
 func main() {
-	var directory string
-	var recursive bool
-	var output string
+  var directory string
+  var recursive bool
+  var output string
 
-	app := cli.NewApp()
-	app.Name = "mklister"
-	app.Usage = "list the contents of a provided directory"
+  app := cli.NewApp()
+  app.Name = "mklister"
+  app.Usage = "list the contents of a provided directory"
 
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name: "path, p",
-			Usage: "path to `DIRECTORY`, required",
-			Destination: &directory,
-		},
-		cli.BoolFlag{
-			Name: "recursive, r",
-			Usage: "when set, list files recursively. default is off",
-			Destination: &recursive,
-		},
-		cli.StringFlag{
-			Name: "output, o",
-			Usage: "json|yml|text, default `FORMAT` is text",
-			Value: "text",
-			Destination: &output,
-		},
-	}
+  app.Flags = []cli.Flag{
+    cli.StringFlag{
+      Name: "path, p",
+      Usage: "path to `DIRECTORY`, required",
+      Destination: &directory,
+    },
+    cli.BoolFlag{
+      Name: "recursive, r",
+      Usage: "when set, list files recursively. default is off",
+      Destination: &recursive,
+    },
+    cli.StringFlag{
+      Name: "output, o",
+      Usage: "json|yml|text, default `FORMAT` is text",
+      Value: "text",
+      Destination: &output,
+    },
+  }
 
-	app.Action = func(c *cli.Context) error {
-		if output == "json" {
-			formattedOutput := []AsJSON{}
+  app.Action = func(c *cli.Context) error {
+    if output == "json" {
+      formattedOutput := []AsJSON{}
 
-			if recursive {
-				formattedOutput = PrepareJSON(directory)
-			} else {
-				formattedOutput = PrepareJSONNonRecursive(directory)
-			}
+      if recursive {
+        formattedOutput = PrepareJSON(directory)
+      } else {
+        formattedOutput = PrepareJSONNonRecursive(directory)
+      }
 
-			marshalled, _ := json.MarshalIndent(formattedOutput, "", " ")
+      marshalled, _ := json.MarshalIndent(formattedOutput, "", " ")
 
-			fmt.Println(string(marshalled))
+      fmt.Println(string(marshalled))
 
-		} else if output == "yaml" {
+    } else if output == "yaml" {
 
-		} else {
-			if recursive {
-				PrintContents(directory, 1)
-			} else {
+    } else {
+      if recursive {
+        PrintContents(directory, 1)
+      } else {
 
-				files, err := ioutil.ReadDir(directory)
+        files, err := ioutil.ReadDir(directory)
 
-				if err != nil {
-					log.Fatal(err)
-				}
+        if err != nil {
+          log.Fatal(err)
+        }
 
-				for _, file := range files {
-					fmt.Println(" " + file.Name())
-				}
-			}
-		}
+        for _, file := range files {
+          fmt.Println(" " + file.Name())
+        }
+      }
+    }
 
-		return nil
-	}
+    return nil
+  }
 
-	app.Run(os.Args)
+  app.Run(os.Args)
 }
